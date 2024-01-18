adf42d5a035c44c896c7ea94783793262b1ab52c
Somil Saxena <somilsaxena100@gmail.com>
2024-01-18 15:48:40 +0530

    third commit

---
+++ data/blogs.js
@@ -0,0 +1,26 @@
+
+    blogs =[
+        {
+            title: "how to get started with Python" ,
+            content: "This is Content Python",
+            slug: "Python-learn"
+        },
+        {
+            title: "how to get started with JS" ,
+            content: "This is Content JS",
+            slug: "JS-learn"
+        },
+        {
+            title: "how to get started with Django" ,
+            content: "This is Content Django",
+            slug: "Django-learn"
+        },
+        {
+            title: "how to get started with CSS" ,
+            content: "This is Content CSS",
+            slug: "CSS-learn"
+        },
+
+    ]
+
+module.exports = blogs;
